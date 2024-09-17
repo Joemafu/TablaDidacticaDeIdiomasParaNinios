8bda0b8ecf54347071875d95b99232ebb16e0176
joemafu <joe_mafu@hotmail.com>
2024-09-17 01:44:26 -0300

    Primer commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'tabla.didactica.de.idiomas',
   appName: 'TablaDidacticaDeIdiomasParaNinios',
   webDir: 'www'
 };
