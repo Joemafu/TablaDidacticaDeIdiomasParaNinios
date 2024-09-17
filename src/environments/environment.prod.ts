1401da1d7b182048d9c55775a76de371e612ce38
joemafu <joe_mafu@hotmail.com>
2024-09-16 21:17:57 -0300

    Primer commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
   production: true
 };
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAryBDDWhnKpgxeaPyS6QRA1YjN9t41HT4",
+  authDomain: "tabladidacticadeidiomas.firebaseapp.com",
+  projectId: "tabladidacticadeidiomas",
+  storageBucket: "tabladidacticadeidiomas.appspot.com",
+  messagingSenderId: "937750634185",
+  appId: "1:937750634185:web:880133f765049837e0b718"
+};
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,29 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { firebaseConfig } from './environments/environment.prod';
+import { provideHttpClient } from '@angular/common/http';
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
+    importProvidersFrom(IonicModule.forRoot({})),
+    provideHttpClient(),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage()),
   ],
-});
+})
+  .catch((err) => console.error(err));
