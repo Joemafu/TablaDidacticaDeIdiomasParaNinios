6940785ddb0cf55f5573c51771db8815e6580fb6
joemafu <joe_mafu@hotmail.com>
2024-09-25 21:35:13 -0300

    Splash

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,41 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, OnInit, inject } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonCardSubtitle, IonCardHeader, IonCard, IonCardTitle, IonCardContent } from '@ionic/angular/standalone';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonButton, IonCardContent, IonCardSubtitle, IonCardTitle, IonCardHeader, IonCard, IonHeader, IonToolbar, IonTitle, IonContent],
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit {
+  
+  authService: AuthService = inject(AuthService);
+  router: Router = inject(Router);
+  usuario: string = '';
+  public subscription: Subscription = new Subscription();
+
+  logout() {
+    this.authService.logout();
+  }
+
+  ngOnInit(): void {
+    this.subscription = this.authService.user$.subscribe((user) => {
+      if (user) {
+        this.authService.currentUserSig.set({
+          mail: user.email!,
+          pass: "",
+          nombre: "", 
+          apellido: "",
+        });
+        this.usuario = user.email!;        
+      } else {
+        this.usuario="";
+        this.authService.currentUserSig.set(null);    
+      }
+    });
+  }
 }
